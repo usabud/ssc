24f649f957bb3f3f54c19606a047c7dd20bd97d6
Budimir <budimir970@gmail.com>
2020-09-05 11:20:11 +0200

    Add jus shop

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
-const http = require('http');
-const os = require('os');
-const fs = require('fs');
+/*
+ * Copyright (c) 2014-2020 Bjoern Kimminich.
+ * SPDX-License-Identifier: MIT
+ */
 
-const hostname = '0.0.0.0';
-const port = 3000;
-
-const greeting = process.env.GREETING || "Hello World"
-const path = process.env.FILE_PATH || '/var/secret/secret.txt'
-
-let fileContent = ""
-fs.access(path, fs.F_OK, (err) => {
-  if (err) {
-    return
-  }
-  fs.readFile(path, 'utf8', function(err, contents) {
-    fileContent = contents;
-  });
+require('./lib/startup/validateDependencies')().then(() => {
+  const server = require('./server')
+  server.start()
 })
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(`${greeting} from ${os.hostname()}\n${fileContent}\n`);
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
